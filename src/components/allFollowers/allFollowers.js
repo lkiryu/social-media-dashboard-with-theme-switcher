a2d58df63d15b9e625f3b370e534bbf975eb0b4c
kiryu <13matheusilva@gmail.com>
2024-05-21 13:42:27 -0300

    allfollowers component map add

--- src/components/allFollowers/allFollowers.js
+++ src/components/allFollowers/allFollowers.js
@@ -3,13 +3,59 @@ import Followers from "../followers/followers";
 
 import './allFollowers.css'
 
+const followersData = [
+    {
+        social: "facebook",
+        user: "@nathanf",
+        total: "1987",
+        text: "Followers",
+        caret: "up",
+        today: "12",
+    },
+    {
+        social: "twitter",
+        user: "@nathanf",
+        total: "1044",
+        text: "Followers",
+        caret: "up",
+        today: "99",
+    },
+    {
+        social: "instagram",
+        user: "@realnathanf",
+        total: "11k",
+        text: "Followers",
+        caret: "up",
+        today: "1099",
+    },
+    {
+        social: "youtube",
+        user: "Nathan F.",
+        total: "8239",
+        text: "Subscribers",
+        caret: "down",
+        today: "144",
+    },
+]
+
 const AllFollowers = () => {
-    return(
+    return (
         <div className="allFollowers dark theme">
-            <Followers social='facebook' user='@nathanf' total='1987' text='Followers' caret='up' today='12'/>
-            <Followers social='twitter' user='@nathanf' total='1044' text='Followers' caret='up' today='99'/>
-            <Followers social='instagram' user='@realnathanf' total='11k' text='Followers' caret='up' today='1099'/>
-            <Followers social='youtube' user='Nathan F.' total='8239' text='Subscribers' caret='down' today='144'/>
+            {
+                followersData.map((item, index) => {
+                    return (
+                        <Followers
+                            key={index}
+                            social={item.social}
+                            user={item.user}
+                            total={item.total}
+                            text={item.text}
+                            caret={item.caret}
+                            today={item.today}
+                        />
+                    )
+                })
+            }
         </div>
     )
 }
--- src/components/followers/followers.js
+++ src/components/followers/followers.js
@@ -2,15 +2,13 @@ import React from "react";
 
 import './followers.css'
 
-const Followers = (props) =>{
-    return(
-        <div className={"Followers "+props.social}>
-            <ul>
-                <li className="user"><i className={'fa-brands fa-'+props.social}></i> {props.user}</li>
-                <li className="total">{props.total}</li>
-                <li className="text">{props.text}</li>
-                <li className={"today "+props.caret}><i className={"fa-solid fa-caret-"+props.caret}></i> {props.today} Today</li>
-            </ul>
+const Followers = (props) => {
+    return (
+        <div className={"Followers " + props.social}>
+            <p className="user"><i className={'fa-brands fa-' + props.social}></i> {props.user}</p>
+            <p className="total">{props.total}</p>
+            <p className="text">{props.text}</p>
+            <p className={"today " + props.caret}><i className={"fa-solid fa-caret-" + props.caret}></i> {props.today} Today</p>
         </div>
     )
 }
