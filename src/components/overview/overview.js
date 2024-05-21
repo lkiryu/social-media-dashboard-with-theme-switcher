6f96277ad772bf2712ef96dc495e8a5b1e207e0d
kiryu <13matheusilva@gmail.com>
2024-05-21 14:31:05 -0300

    overview component map add

--- src/components/overview/overview.js
+++ src/components/overview/overview.js
@@ -3,20 +3,85 @@ import Card from "../card/card";
 
 import './overview.css'
 
+const cardData = [
+    {
+        cardTitle: "Page Views",
+        social: "facebook",
+        value: "87",
+        caret: "up",
+        today: "3%",
+    },
+    {
+        cardTitle: "Likes",
+        social: "facebook",
+        value: "52",
+        caret: "down",
+        today: "2%",
+    },
+    {
+        cardTitle: "Likes",
+        social: "instagram",
+        value: "5462",
+        caret: "up",
+        today: "2257%",
+    },
+    {
+        cardTitle: "Profile Views",
+        social: "instagram",
+        value: "52k",
+        caret: "up",
+        today: "1375%",
+    },
+    {
+        cardTitle: "Retweets",
+        social: "twitter",
+        value: "117",
+        caret: "up",
+        today: "303%",
+    },
+    {
+        cardTitle: "Likes",
+        social: "twitter",
+        value: "507",
+        caret: "up",
+        today: "553%",
+    },
+    {
+        cardTitle: "Likes",
+        social: "youtube",
+        value: "107",
+        caret: "down",
+        today: "19%",
+    },
+    {
+        cardTitle: "Total Views",
+        social: "youtube",
+        value: "1407",
+        caret: "down",
+        today: "12%",
+    },
+]
+
 const Overview = () =>{
     return (
         <section className="overview dark theme">
             <h2 className="title">Overview - Today</h2>
 
             <div className="cards">
-                <Card cardTitle='Page Views' social='facebook' value='87' caret='up' today='3%'/>
-                <Card cardTitle='Likes' social='facebook' value='52' caret='down' today='2%'/>
-                <Card cardTitle='Likes' social='instagram' value='5462' caret='up' today='2257%'/>
-                <Card cardTitle='Profile Views' social='instagram' value='52k' caret='up' today='1375%'/>
-                <Card cardTitle='Retweets' social='twitter' value='117' caret='up' today='303%'/>
-                <Card cardTitle='Likes' social='twitter' value='507' caret='up' today='553%'/>
-                <Card cardTitle='Likes' social='youtube' value='107' caret='down' today='19%'/>
-                <Card cardTitle='Total Views' social='youtube' value='1407' caret='down' today='12%'/>
+                {
+                    cardData.map((item, index)=>{
+                        return(
+                            <Card 
+                                key={index}
+                                cardTitle={item.cardTitle}
+                                social={item.social}
+                                value={item.value}
+                                caret={item.caret}
+                                today={item.today}
+                            />
+                        )
+                    })
+                }
             </div>
         </section>
     )
